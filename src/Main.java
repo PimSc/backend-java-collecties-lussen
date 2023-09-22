21ef3dbf9eeee1e5a3b23e7372ed826d48d1f3e6
PimSc <pimschuitema@gmail.com>
2023-09-22 12:49:52 +0200

    huiswerk gemaakt

--- src/Main.java
+++ src/Main.java
@@ -4,45 +4,54 @@ public class Main {
     public static void main(String[] args) {
 
         // Hier mag je je code schrijven voor de hoofd-opdracht
-
-        Integer[] numeric = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-        String[] alphabetic = {"een", "twee", "vijf", "vier", "drie", "zes", "zeven", "negen", "acht", "nul"};
-
-        Translator t1 = new Translator(numeric, alphabetic);
-
-        System.out.println(t1.translate(1));
-
-        boolean play = true;
-        String ongeldig = "ongeldige invoer";
-        Scanner scanner = new Scanner(System.in);
-
-        while (play) {
-            System.out.println("\nType 'x' om te stoppen \nType 'v' om te vertalen");
-            String input = scanner.nextLine();
-
-            if (input.equals("x")) {
-                play = false;
-            } else if (input.equals("v")) {
-                System.out.println("Type een cijfer in van 0 t/m 9");
-                int number = scanner.nextInt();
-                scanner.nextLine();
-
-                if (number < 10) {
-                    String result = t1.translate(number);
-                    System.out.println("De vertaling van " + number + " is " + result);
-                } else {
-                    System.out.println(ongeldig);
-                }
-            }
-            else {
-                System.out.println("ongeldig");
-            }
+//        Zet alle grijze code aan om de gewone opdracht te laten werken.
 
 
 
-        }
+
+
+
+//        Integer[] numeric = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+//        String[] alphabetic = {"een", "twee", "vijf", "vier", "drie", "zes", "zeven", "negen", "acht", "nul"};
+//
+//        Translator t1 = new Translator(numeric, alphabetic);
+//
+//        System.out.println(t1.translate(1));
+//
+//        boolean play = true;
+//        String ongeldig = "ongeldige invoer";
+//        Scanner scanner = new Scanner(System.in);
+//
+//        while (play) {
+//            System.out.println("\nType 'x' om te stoppen \nType 'v' om te vertalen");
+//            String input = scanner.nextLine();
+//
+//            if (input.equals("x")) {
+//                play = false;
+//            } else if (input.equals("v")) {
+//                System.out.println("Type een cijfer in van 0 t/m 9");
+//                int number = scanner.nextInt();
+//                scanner.nextLine();
+//
+//                if (number < 10) {
+//                    String result = t1.translate(number);
+//                    System.out.println("De vertaling van " + number + " is " + result);
+//                } else {
+//                    System.out.println(ongeldig);
+//                }
+//            } else {
+//                System.out.println("ongeldig");
+//            }
+//        }
+
+
+//        Bonus opdracht hieronder
+
+        Integer[] numeric = {7, 3, 3, 8};
+        String[] alphabetic = {"zeven", "drie", "vijf", "vier", "drie", "zes", "zeven", "negen", "acht", "nul"};
+
+
+
     }
-
-
 }
 
